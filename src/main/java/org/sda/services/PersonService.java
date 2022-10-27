c225f6567872012f17bf626004f831e2a6443013
Cepblu-tyt <sergei.oksanen@gmail.com>
2022-10-27 19:57:59 +0300

    Exercise

--- src/main/java/org/sda/services/PersonService.java
+++ src/main/java/org/sda/services/PersonService.java
@@ -1,5 +1,7 @@
 package org.sda.services;
 
+import org.sda.models.Person;
+
 /**
  * PersonService interface to  handle Person related operations
  *
@@ -15,6 +17,14 @@ public interface PersonService {
      */
 
     int getPersonBirthYear(int age);
+
+    /**
+     * to get the full name of person
+     *
+     * @param person Person modell
+     * @return Full name as String
+     */
+    String getPersonFullName(Person person);
 
     //static method is allowed
     static int getAverageAge() {
---
+++ src/main/java/org/sda/services/implementations/PersonServiceImpl.java
@@ -0,0 +1,21 @@
+package org.sda.services.implementations;
+
+import org.sda.models.Person;
+import org.sda.services.PersonService;
+
+/**
+ * Implementation of person service
+ *
+ * @author Sergei Oksanen
+ */
+public class PersonServiceImpl implements PersonService {
+    @Override
+    public int getPersonBirthYear(int age) {
+        return 0;
+    }
+
+    @Override
+    public String getPersonFullName(Person person) {
+        return null;
+    }
+}
